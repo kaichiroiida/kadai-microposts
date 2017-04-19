d834ecc42c71db3f11ed7122c81a6fa92fbcb4c2
kaichiroiida <s15mm302@gmail.com>
2017-04-19 05:48:00 +0000

    timeline

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,10 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy
+    def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_path
   end
 end
   private
