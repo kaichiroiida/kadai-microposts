3e67c762ff5d0f47853700943f1b5937d7e90a9b
kaichiroiida <s15mm302@gmail.com>
2017-04-20 00:52:37 +0000

    favorite

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,18 @@
+class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+  
+  
+  def create
+    post = Micropost.find(params[:micropost_id])
+    current_user.favo(post)
+    flash[:success] = 'Postをお気に入り登録しました。'
+    redirect_to post
+  end
+
+  def destroy
+    post = Micropost.find(params[:micropost_id])
+    current_user.unfavo(post)
+    flash[:success] = 'Postのお気に入り登録を解除しました。'
+    redirect_to post
+  end
+end
